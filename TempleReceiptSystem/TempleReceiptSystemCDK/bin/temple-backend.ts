f88fde0ac8d88a97ba7bc035c425a0b938e127dd
pranavnawathe <pranavnawathe@users.noreply.github.com>
2026-02-24 22:08:19 -0800

    dummy commit

--- TempleReceiptSystem/TempleReceiptSystemCDK/bin/temple-backend.ts
+++ TempleReceiptSystem/TempleReceiptSystemCDK/bin/temple-backend.ts
@@ -10,7 +10,7 @@ const env = {
   account: '671924214635',
 };
 
-// CDK Pipeline (self-mutating)
+// CDK Pipeline(self-mutating)
 new PipelineStack(app, 'TempleReceiptPipelineStack', {
   env,
   connectionArn: 'arn:aws:codeconnections:ap-south-1:671924214635:connection/81131386-f3f1-4dfd-9085-a8a70db091ae',
